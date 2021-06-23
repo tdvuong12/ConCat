449a75290f8e56b24278587b13677083ba93829e
Nguyen Thuan Hung <77202230+Hungkhoaitay@users.noreply.github.com>
2021-06-23 22:32:39 +0800

    No vtd

--- app/src/main/java/com/example/myapplication/FloatingViewService.java
+++ app/src/main/java/com/example/myapplication/FloatingViewService.java
@@ -24,7 +24,7 @@ public class FloatingViewService extends Service
      * mFloatingView: Design, action, and algorithms relating to the floating widget
      * collapsedView:
      * expandedView:
-     vtd*/
+     */
 
     private WindowManager mWindowManager;
     private View mFloatingView;
