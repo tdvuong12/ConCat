3fa16bb18ce0dbe7ea0b2dc94b7bbc501838f7c9
hmanhduc2k <hmanhduc2k@gmail.com>
2021-06-24 11:26:44 +0800

    add ConCat

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,17 +12,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
- public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+ public class MainActivity extends AppCompatActivity
+         implements View.OnClickListener, View.OnLongClickListener {
 
     Button[] buttonList;
 
     private static final int WINDOW_PERMISSION = 123;
     private static boolean PERMISSION_GRANTED = true;
-
-    private ApplicationInfo[] appList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +27,22 @@ import java.util.List;
 
         final AppInfo emptyApp = AppInfo.none(this);
 
+        Button conCat = findViewById(R.id.ConCat);
+        conCat.setOnClickListener(this);
         Button firstBtn = findViewById(R.id.firstButton);
         Button secondBtn = findViewById(R.id.secondButton);
         Button thirdBtn = findViewById(R.id.thirdButton);
         Button fourthBtn = findViewById(R.id.fourthButton);
 
-        buttonList = new Button[] { firstBtn, secondBtn, thirdBtn, fourthBtn };
+        buttonList = new Button[]{firstBtn, secondBtn, thirdBtn, fourthBtn};
 
-        for (Button btn: buttonList) {
+        for (Button btn : buttonList) {
             emptyApp.setButton(btn);
-    private void askForSystemOverlayPermission(){
+        }
+    }
+
+
+    private void askForSystemOverlayPermission() {
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:" + getPackageName()));
         startActivityForResult(intent, WINDOW_PERMISSION);
@@ -48,6 +50,7 @@ import java.util.List;
             this.PERMISSION_GRANTED = true;
         }
     }
+
 
     @Override
     public void onClick(View v) {
@@ -64,29 +67,13 @@ import java.util.List;
                     askForSystemOverlayPermission();
                 }
                 break;
-            case R.id.firstButton:
-                // launchApp(filteredPackages.get(4));
-                break;
-            case R.id.secondButton:
-                // Toast.makeText(this, "firstClicked", Toast.LENGTH_SHORT).show();
-                display.setText("secondClicked");
-                startActivity(new Intent(this, ScrollingActivity.class));
-                break;
-            case R.id.thirdButton:
-                // Toast.makeText(this, "firstClicked", Toast.LENGTH_SHORT).show();
-                display.setText("thirdClicked");
-                break;
-            case R.id.fourthButton:
-                // Toast.makeText(this, "firstClicked", Toast.LENGTH_SHORT).show();
-                display.setText("fourthClicked");
-                break;
             default:
                 break;
         }
     }
 
-    @Override
-    public boolean onLongClick(View v) {
-        return false;
-    }
-}
+     @Override
+     public boolean onLongClick(View v) {
+         return false;
+     }
+ }
